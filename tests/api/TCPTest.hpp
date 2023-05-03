9cd5b20a2966a453ac5cd2d1a916be306ecb8459
Ivaschenko Dmitry <ivaschenkoda144@gmail.com>
2023-05-04 00:04:22 +0600

    linux build update

--- tests/api/TCPTest.hpp
+++ tests/api/TCPTest.hpp
@@ -11,42 +11,41 @@
 class TCPTest: public ::testing::Test
 {
 public:
-    TCPTest() {
-        m_settings = net::settings::SockSetting{
-			net::settings::aifamily::inetv4,
-			net::settings::aisocktype::stream,
-			net::settings::aiprotocol::tcp,
-			net::settings::aiflags::passive,
-			10ul
-		};
-    }
+    TCPTest() {}
     ~TCPTest() {}
 
     net::socket_t createTCPServer()
     {
-        return net::make_server(m_settings, m_address, m_port);
+        return net::make_server(m_server, m_address, m_port);
     }
 
     net::socket_t createTCPConnection()
     {
-        return net::make_connection(m_settings, m_address, m_connectionPort);
+        return net::make_connection(m_connection, m_address, m_connectionPort);
     }
 
 protected:
     void SetUp() override
     {
-        m_settings = net::settings::SockSetting{
+        m_server = net::settings::server_t{
 			net::settings::aifamily::inetv4,
 			net::settings::aisocktype::stream,
 			net::settings::aiprotocol::tcp,
 			net::settings::aiflags::passive,
 			10ul
 		};
+        m_connection = net::settings::connection_t{
+			net::settings::aifamily::inetv4,
+			net::settings::aisocktype::stream,
+			net::settings::aiprotocol::tcp,
+			net::settings::aiflags::passive
+		};
     }
 
 private:
     const int32_t messageLimit = 5;
-    net::settings::SockSetting m_settings;
+    net::settings::server_t m_server;
+    net::settings::connection_t m_connection;
     const char* m_address = "127.0.0.1";
 	const char* m_connectionPort = "3000";
 	const int32_t m_port = 3000;
